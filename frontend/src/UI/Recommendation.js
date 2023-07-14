962e9856a47164e36595b0028e33adecc274f399
JUAN <erickuniv@gmail.com>
2023-07-14 23:44:39 +0700

    no message

--- frontend/src/UI/Recommendation.js
+++ frontend/src/UI/Recommendation.js
@@ -7,13 +7,13 @@ export const Recommendation = (props) => {
       </div>
       <div className={classes['uphead']}>
         <a href='#' className={classes['upimg']}>
-      <img src="./assets/BookCover.png" alt="" />
+      <img src="./assets/Rectangle128.png" alt="" />
         </a>
       </div>
       <div className={classes['uptext']}>
         <div className={classes['upintext']}>
-      <span>Rekomendasi hari ini :</span>
-      <h1> Kimia XII</h1>
+      <span>Today's Recommendation :</span>
+      <h1> Percy Jackson and the Olympians </h1>
       <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </span>
       
         </div>
